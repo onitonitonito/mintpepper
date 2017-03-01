6dfa920d0fd2798d05462726a0ac6d327092c6e0
Kay the Home PC <nightpapy@naver.com>
2017-03-01 13:48:04 +0900

    3-Modify Python Script Using Print Format

--- python.py
+++ python.py
@@ -1,2 +1,9 @@
-print ("Hello")
-#fgakjdsfgkasjhfdkasjdfhgkasjdgfaksjdfkagjdsfkhgasjdgfkjasfkdgasjdfkgjasfkdgajsfhgkasjdfhgkasjfdgkajsfkdgajskfdgasjfkdgajsgfkdajsfkdgajh
+# Python TEST Script = 2017.3/1(Wed)
+#
+
+favorite =["Apple","Pizza","PASTA","P-P-A-P","Freak","Undertale"]
+print ("Hello Python World!! \n-----------------------\n")
+for i in range(0,6):
+    print(end=" \t")
+    print(i,end=". ")
+    print(favorite[i])
